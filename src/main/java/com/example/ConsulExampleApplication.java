fe9be8fb34b924770450f6a901965b6726a3f327
Karthikeyan Balasubramaniam <nklkarthi@gmail.com>
2016-09-18 17:28:55 +0800

    Add code from https://cloud.spring.io/spring-cloud-consul/

--- src/main/java/com/example/ConsulExampleApplication.java
+++ src/main/java/com/example/ConsulExampleApplication.java
@@ -1,12 +1,24 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+
 public class ConsulExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsulExampleApplication.class, args);
-	}
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConsulExampleApplication.class).web(true).run(args);
+    }
+
 }
